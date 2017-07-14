bb6c60cb437c9439c0cdf1d84718389fa8eec7b6
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-07-13 20:07:50 -0400

    Adding configurations for Heoku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+var connection = mysql.createConnection(process.env.JAWS_DB_URL);  
+} else {
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -8,7 +11,7 @@ var connection = mysql.createConnection({
   password: "",
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
